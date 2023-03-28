86c814e619cda6e27b81c3fb64c03a9dca41f1cd
Evan Sosenko <razorx@evansosenko.com>
2023-03-28 16:50:06 -0700

    Prefer environmentVariables over process.env

--- ava.config.js
+++ ava.config.js
@@ -1,12 +1,11 @@
-import process from 'node:process'
-
 export default () => {
-  // UPSTREAM: https://nodejs.org/docs/latest-v18.x/api/esm.html#loaders
-  process.env.NODE_NO_WARNINGS = '1'
-
   return {
     ignoredByWatcher: ['tmp/**/*'],
     files: ['**/*.spec.ts', '!package/**/*'],
+    environmentVariables: {
+      // UPSTREAM: https://nodejs.org/docs/latest-v18.x/api/esm.html#loaders
+      NODE_NO_WARNINGS: '1'
+    },
     extensions: {
       ts: 'module'
     },
