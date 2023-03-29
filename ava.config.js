e5ea1de2a4914ac86daf691160868b15dd842eec
Evan Sosenko <razorx@evansosenko.com>
2023-03-28 17:41:16 -0700

    Use .test over .spec

--- ava.config.js
+++ ava.config.js
@@ -1,7 +1,7 @@
 export default () => {
   return {
     ignoredByWatcher: ['tmp/**/*'],
-    files: ['**/*.spec.ts', '!package/**/*'],
+    files: ['**/*.test.ts', '!package/**/*'],
     environmentVariables: {
       // UPSTREAM: https://nodejs.org/docs/latest-v18.x/api/esm.html#loaders
       NODE_NO_WARNINGS: '1'
